fb89480308e2d164d3da4ed23c0f4e7e4773e386
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-05-31 02:25:34 +0900

    edit_index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,11 +1,12 @@
 $(function () {
-  $('.card-image').hover(
-    function () {
-      $(this).css("opacity", "0.3");
-    },
-    function () {
-      $(this).css("opacity", "1");
-    }
+  document.addEventListener('turbolinks:load', function () {
+    $('.card-image').hover(
+      function () {
+        $(this).css("opacity", "0.3");
+      },
+      function () {
+        $(this).css("opacity", "1");
+      }
     );
     $(".card-image").click(function(){
       var $content = $(".card-content").find("p")
@@ -13,10 +14,36 @@ $(function () {
       if ($content.hasClass("open")){
         $content.removeClass("open")
         $content.slideUp();
+
       }else{
         $content.addClass("open")
         $content.slideDown();
       };
 
     });
+
+    $(".submit-btn").hover(
+      function(){
+        $(this).css("opacity", "0.9");
+        $(this).animate({
+          "height": "100px",
+          "width": "100px"
+        },200)
+      },
+      function(){
+        $(this).css("opacity", "1");
+        $(this).animate({
+          "height": "90px",
+          "width": "90px"
+        },200)
+      }
+    );
+
+    $(".new-content-post-index_text").click(function(){
+      $(this).animate({
+        "width": "555px"
+      }, 200)
+
+    });
   });
+});
